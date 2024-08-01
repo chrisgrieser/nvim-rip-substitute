a0b0643453649077c592e53358ffd2fdb2ac8f6e
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-08-01 17:01:57 +0200

    fix: notifications with nvim-notify use title from config

--- lua/rip-substitute/utils.lua
+++ lua/rip-substitute/utils.lua
@@ -4,8 +4,9 @@ local M = {}
 ---@param msg string
 ---@param level? "info"|"trace"|"debug"|"warn"|"error"
 function M.notify(msg, level)
+	local config = require("rip-substitute.config").config
 	if not level then level = "info" end
-	vim.notify(msg, vim.log.levels[level:upper()], { title = "rip-substitute" })
+	vim.notify(msg, vim.log.levels[level:upper()], { title = config.popupWin.title })
 end
 
 ---@return number startLnum
